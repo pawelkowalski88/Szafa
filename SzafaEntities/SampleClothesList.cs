f6ad7e36804bc7525b75b05251aefd60b3fa516a
Paweł Kowalski <pavlozzz@live.com>
2018-02-03 09:53:11 +0100

    Sample ClothesList added

---
+++ SzafaEntities/SampleClothesList.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseConnectionSQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzafaEntities
+{
+    public class SampleClothesList
+    {
+        public List<clothes> ClothesList
+        {
+            get
+            {
+                return new List<clothes>()
+                {
+                    new clothes()
+                    {
+                        name = "Biała koszula",
+                        times_on = 5
+                    },
+                    new clothes()
+                    {
+                        name = "Spodnie jeansowe",
+                        times_on = 2
+                    }
+                };
+            }
+        }
+
+        public bool Updating
+        {
+            get { return false; }
+        }
+    }
+}
